8623620f6e794e9315b2d5c19ed9f3df47e36879
David9191 <jisung9105@gmail.com>
2023-04-07 00:17:26 +0900

    Separate files in udemy/node/4-natours/starter following MVC model

---
+++ udemy/node/4-natours/starter/routes/tourRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const tourController = require('../controller/tourController');
+
+router.param('id', tourController.checkID);
+
+// Create a checkBody middleware
+// Check if body contains the name and price property
+// If not, send back 400 (bad request)
+// Add it to the post handler stack
+
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  // checkBody에서 next하면 createTour로 간다.
+  .post(tourController.checkBody, tourController.createTour);
+
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
+
+module.exports = router;
