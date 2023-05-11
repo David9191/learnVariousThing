f988dcddc2fa09885fd6b284bf8e2bb6c5e9ddfe
David9191 <jisung9105@gmail.com>
2023-05-12 02:14:02 +0900

    Feat: implement TestProps component

---
+++ react-official-document/src/JSX/TodoList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TodoList = () => {
+  return (
+    <div>
+      <h1>Ji's Todos</h1>
+      <img src="https://i.imgur.com/yXOvdOSs.jpg" alt="Ji" class="photo" />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
