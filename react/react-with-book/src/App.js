819a50fb2b6cd0fbc559e1959074e79d8230227c
David9191 <jisung9105@gmail.com>
2023-05-18 05:53:51 +0900

    Learn: 공부 9장 컴포넌트 스타일링

--- react/react-with-book/src/App.js
+++ react/react-with-book/src/App.js
@@ -3,15 +3,12 @@ import './App.css';
 import Counter from './Hooks/Counter';
 import Info from './Hooks/Info';
 import Average from './Hooks/Average';
+import CSSModule from './styling/CSSModule';
 
 const App = () => {
   return (
     <>
-      <Counter />
-      <br />
-      <Info />
-      <br />
-      <Average />
+      <CSSModule />
     </>
   );
 };
