e0f75fadf6011920d398eb37afc70bfb75b72746
David9191 <jisung9105@gmail.com>
2023-05-17 14:57:00 +0900

    Learn: Learn 책 6장

--- react/react-official-document/src/App.tsx
+++ react/react-official-document/src/App.tsx
@@ -1,23 +1,23 @@
-import React, { useState } from "react";
-import "./App.css";
-import TestProps from "./Jsx/TestProps";
+import React, { useState } from 'react';
+import './App.css';
+import TestProps from './Jsx/TestProps';
 
 const App = () => {
-  return <TestProps person={{ name: "Ji", age: 24 }} size={185} />;
+  return <TestProps person={{ name: 'Ji', age: 24 }} size={185} />;
 };
 
 const products = [
-  { title: "Cabbage", isFruit: false, id: 1 },
-  { title: "Garlic", isFruit: false, id: 2 },
-  { title: "Apple", isFruit: true, id: 3 },
+  { title: 'Cabbage', isFruit: false, id: 1 },
+  { title: 'Garlic', isFruit: false, id: 2 },
+  { title: 'Apple', isFruit: true, id: 3 },
 ];
 
 const ShoppingList = () => {
-  const listItems = products.map(product => (
+  const listItems = products.map((product) => (
     <li
       key={product.id}
       style={{
-        color: product.isFruit ? "magenta" : "darkgreen",
+        color: product.isFruit ? 'magenta' : 'darkgreen',
       }}
     >
       {product.title}
